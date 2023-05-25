3bbdaa46b9a922b3c2a17ea75dff80c13d90548b
kimimarux <DenisThuerling@outlook.de>
2023-05-25 20:19:19 +0200

    Change All

---
+++ my-app/src/routes/data.js
@@ -0,0 +1,8 @@
+export const recipes = [
+	{
+		slug: 'recipe1',
+		title: 'Thai red curry with chicken!',
+		content:
+			'<p>Heat the oil in a large skillet over medium-high heat. Add the curry paste and fry for about 2 minutes until fragrant. Add half of the coconut milk and bring to a boil. Add the chicken pieces and simmer for about 15 minutes until cooked through.Add the fish sauce, sugar, water, bamboo shoots, and kaffir lime leaves and stir well.Simmer for another 5 minutes until slightly thickened.Stir in the remaining coconut milk and turn off the heat. Add the basil leaves and chili slices if using.Serve hot with steamed rice or noodles.</p>'
+	}
+];
